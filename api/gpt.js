97f2fea963a24dc9b6bb1005ce72d0e5e7ba27e3
husanr <1090037611@qq.com>
2023-10-08 15:43:07 +0800

    feat: upload to github

---
+++ api/gpt.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+
+axios.defaults.headers.post['Content-Type'] = 'text/xml';
+
+module.exports = async (req, res) => {
+  const er = req.query.echostr
+  // 将用户消息发送到现有的后端接口
+  try {
+    // const response = await axios.post('https://tqs3ft.laf.dev/test', req );
+    const response = await axios({
+      url: "https://cpq9b7mect.hk.aircode.run/hello",
+      method: req.method,
+      params: req.query,
+      data: req
+    })
+    // console.log('res data',response.data);
+    console.log('echostr',er);
+    // 处理后端接口的响应，并将其返回给前端
+    if(er) {
+      res.status(200).end(er);
+    } else {
+      res.status(200).end(response.data);
+    }
+  } catch (error) {
+    // 处理转发请求失败的情况
+    console.error(error);
+    res.status(500).json({ error: '转发请求失败' });
+  }
+};
